c3059ac6f75d6093fe3cccb7113556f6f91f29fa
Filipe Toscano <filipe.toscano@gmail.com>
2022-03-28 23:33:31 +0100

    Add tool to match ISO3166 alpha-2 with the invoicexpress name

---
+++ tools/InvoiceXpress.CountryGen/Country.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InvoiceXpress.CountryGen;
+
+/// <summary />
+public class Country
+{
+    /// <summary />
+    [JsonPropertyName( "name" )]
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// 2 letter code.
+    /// </summary>
+    [JsonPropertyName( "alpha-2" )]
+    public string Alpha2 { get; set; } = default!;
+
+    /// <summary>
+    /// 3 letter code.
+    /// </summary>
+    [JsonPropertyName( "alpha-3" )]
+    public string Alpha3 { get; set; } = default!;
+
+    /// <summary>
+    /// Three digit country-code.
+    /// </summary>
+    [JsonPropertyName( "country-code" )]
+    public string CountryCode { get; set; } = default!;
+
+    /// <summary>
+    /// Region
+    /// </summary>
+    [JsonPropertyName( "region" )]
+    public string Region { get; set; } = default!;
+
+    /// <summary>
+    /// Region
+    /// </summary>
+    [JsonPropertyName( "sub-region" )]
+    public string SubRegion { get; set; } = default!;
+}
