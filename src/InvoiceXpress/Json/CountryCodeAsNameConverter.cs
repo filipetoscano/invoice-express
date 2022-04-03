9b9377785e7b2eae8aded0a2ff18e64393bf5fe6
Filipe Toscano <filipe.toscano@gmail.com>
2022-04-03 11:29:28 +0100

    Add country mapper

---
+++ src/InvoiceXpress/Json/CountryCodeAsNameConverter.cs
@@ -0,0 +1,36 @@
+﻿using InvoiceXpress.Map;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace InvoiceXpress.Json;
+
+/// <summary />
+public class CountryCodeAsNameConverter : JsonConverter<string>
+{
+    /// <summary />
+    public override string Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+    {
+        if ( reader.TokenType != JsonTokenType.String )
+            throw new JsonException( "Expected string" );
+
+        var name = reader.GetString()!;
+        var country = IXCountry.Map.SingleOrDefault( x => x.Name == name );
+
+        if ( country == null )
+            throw new JsonException( $"Country { name } is not supported" );
+
+        return country.Code;
+    }
+
+
+    /// <summary />
+    public override void Write( Utf8JsonWriter writer, string value, JsonSerializerOptions options )
+    {
+        var country = IXCountry.Map.SingleOrDefault( x => x.Code == value );
+
+        if ( country == null )
+            throw new JsonException( $"Country { value } is not supported" );
+
+        writer.WriteStringValue( country.Name );
+    }
+}
---
+++ src/InvoiceXpress/Map/IXCountry.cs
@@ -0,0 +1,22 @@
+﻿namespace InvoiceXpress.Map;
+
+/// <summary />
+/// <remarks>
+/// List of countries as per: https://invoicexpress.com/api-v2/documentation/appendix
+/// </remarks>
+internal partial class IXCountry
+{
+    /// <summary />
+    internal IXCountry( string code, string name )
+    {
+        Code = code;
+        Name = name;
+    }
+
+
+    /// <summary />
+    internal string Code { get; set; } = default!;
+
+    /// <summary />
+    internal string Name { get; set; } = default!;
+}
