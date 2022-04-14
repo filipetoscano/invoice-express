a0448f736e3aea161f5690cb9dcb2ab1280f2d69
Filipe Toscano <filipe.toscano@gmail.com>
2022-04-14 15:11:03 +0100

    Improve pdf commands: retry until file is available, add variant which downloads PDF file

--- tools/InvoiceXpress.Cli/Estimate/EstimatePdfCommand.cs
+++ tools/InvoiceXpress.Cli/Estimate/EstimatePdfCommand.cs
@@ -1,5 +1,6 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace InvoiceXpress.Cli;
 
@@ -17,16 +18,80 @@ public class EstimatePdfCommand
     [Required]
     public int? EstimateId { get; set; }
 
+    /// <summary />
+    [Option( "-u|--url", CommandOptionType.NoValue, Description = "Emit URL to console, don't download PDF document" )]
+    public bool UrlOnly { get; set; } = false;
+
+    /// <summary />
+    [Option( "-o|--output-file", CommandOptionType.SingleValue, Description = "Name of file to write to" )]
+    public string? OutputFile { get; set; }
+
 
     /// <summary />
     private async Task<int> OnExecuteAsync( InvoiceXpressClient api, IConsole console )
     {
-        var res = await api.EstimatePdfGenerateAsync( this.EstimateType!.Value, this.EstimateId!.Value );
+        if ( this.UrlOnly == true )
+            return await PdfGenerateAsync( api, console );
+        else
+            return await PdfDownloadAsync( api, console );
+    }
 
-        if ( res.IsSuccessful == false )
-            return console.WriteError( res );
 
-        Console.WriteLine( "Url: {0}", res.Result!.Url );
+    /// <summary />
+    private async Task<int> PdfGenerateAsync( InvoiceXpressClient api, IConsole console )
+    {
+        string url;
+
+        while ( true )
+        {
+            var res = await api.EstimatePdfGenerateAsync( this.EstimateType!.Value, this.EstimateId!.Value );
+
+            if ( res.IsSuccessful == false )
+                return console.WriteError( res );
+
+            if ( res.StatusCode == HttpStatusCode.OK )
+            {
+                url = res.Result!.Url;
+                break;
+            }
+
+            await Task.Delay( 1000 );
+        }
+
+        Console.WriteLine( url );
+        return 0;
+    }
+
+
+    /// <summary />
+    private async Task<int> PdfDownloadAsync( InvoiceXpressClient api, IConsole console )
+    {
+        byte[] bytes;
+
+        while ( true )
+        {
+            var res = await api.EstimatePdfDocumentAsync( this.EstimateType!.Value, this.EstimateId!.Value );
+
+            if ( res.IsSuccessful == false )
+                return console.WriteError( res );
+
+            if ( res.StatusCode == HttpStatusCode.OK )
+            {
+                bytes = res.Result!;
+                break;
+            }
+
+            await Task.Delay( 1000 );
+        }
+
+
+        /*
+         * 
+         */
+        var filename = this.OutputFile ?? $"{ this.EstimateType!.Value }-{ this.EstimateId!.Value }.pdf";
+
+        Console.WriteLine( $"Writing to { filename }..." );
+        await File.WriteAllBytesAsync( filename, bytes );
 
         return 0;
     }
--- tools/InvoiceXpress.Cli/Guide/GuidePdfCommand.cs
+++ tools/InvoiceXpress.Cli/Guide/GuidePdfCommand.cs
@@ -1,5 +1,6 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace InvoiceXpress.Cli;
 
@@ -17,16 +18,80 @@ public class GuidePdfCommand
     [Required]
     public int? GuideId { get; set; }
 
+    /// <summary />
+    [Option( "-u|--url", CommandOptionType.NoValue, Description = "Emit URL to console, don't download PDF document" )]
+    public bool UrlOnly { get; set; } = false;
+
+    /// <summary />
+    [Option( "-o|--output-file", CommandOptionType.SingleValue, Description = "Name of file to write to" )]
+    public string? OutputFile { get; set; }
+
 
     /// <summary />
     private async Task<int> OnExecuteAsync( InvoiceXpressClient api, IConsole console )
     {
-        var res = await api.GuidePdfGenerateAsync( this.GuideType!.Value, this.GuideId!.Value );
+        if ( this.UrlOnly == true )
+            return await PdfGenerateAsync( api, console );
+        else
+            return await PdfDownloadAsync( api, console );
+    }
 
-        if ( res.IsSuccessful == false )
-            return console.WriteError( res );
 
-        Console.WriteLine( res.Result!.Url );
+    /// <summary />
+    private async Task<int> PdfGenerateAsync( InvoiceXpressClient api, IConsole console )
+    {
+        string url;
+
+        while ( true )
+        {
+            var res = await api.GuidePdfGenerateAsync( this.GuideType!.Value, this.GuideId!.Value );
+
+            if ( res.IsSuccessful == false )
+                return console.WriteError( res );
+
+            if ( res.StatusCode == HttpStatusCode.OK )
+            {
+                url = res.Result!.Url;
+                break;
+            }
+
+            await Task.Delay( 1000 );
+        }
+
+        Console.WriteLine( url );
+        return 0;
+    }
+
+
+    /// <summary />
+    private async Task<int> PdfDownloadAsync( InvoiceXpressClient api, IConsole console )
+    {
+        byte[] bytes;
+
+        while ( true )
+        {
+            var res = await api.GuidePdfDocumentAsync( this.GuideType!.Value, this.GuideId!.Value );
+
+            if ( res.IsSuccessful == false )
+                return console.WriteError( res );
+
+            if ( res.StatusCode == HttpStatusCode.OK )
+            {
+                bytes = res.Result!;
+                break;
+            }
+
+            await Task.Delay( 1000 );
+        }
+
+
+        /*
+         * 
+         */
+        var filename = this.OutputFile ?? $"{ this.GuideType!.Value }-{ this.GuideId!.Value }.pdf";
+
+        Console.WriteLine( $"Writing to { filename }..." );
+        await File.WriteAllBytesAsync( filename, bytes );
 
         return 0;
     }
