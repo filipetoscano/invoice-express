7038279b1dfff4043b63b034217f80b79a2884e7
Filipe Toscano <filipe.toscano@gmail.com>
2022-04-16 22:44:09 +0100

    Add --all and --json flags to client list command

--- tools/InvoiceXpress.Cli/Client/ClientListCommand.cs
+++ tools/InvoiceXpress.Cli/Client/ClientListCommand.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleTables;
 using McMaster.Extensions.CommandLineUtils;
+using System.Text.Json;
 
 namespace InvoiceXpress.Cli;
 
@@ -15,31 +16,84 @@ public class ClientListCommand
     [Option( "-s|--page-size", CommandOptionType.SingleValue, Description = "Page size" )]
     public int PageSize { get; set; } = 20;
 
+    /// <summary />
+    [Option( "-a|--all", CommandOptionType.NoValue, Description = "Fetch all records" )]
+    public bool FetchAll { get; set; }
+
+    /// <summary />
+    [Option( "--json", CommandOptionType.NoValue, Description = "Emit results as JSON" )]
+    public bool EmitJson { get; set; }
+
 
     /// <summary />
     private async Task<int> OnExecuteAsync( InvoiceXpressClient api, IConsole console )
     {
-        var res = await api.ClientListAsync( this.Page, this.PageSize );
+        /*
+         * 
+         */
+        List<Client> clients;
+        Pagination? pagination = null;
 
-        if ( res.IsSuccessful == false )
-            return console.WriteError( res );
+        if ( this.FetchAll == false )
+        {
+            var res = await api.ClientListAsync( this.Page, this.PageSize );
+
+            if ( res.IsSuccessful == false )
+                return console.WriteError( res );
+
+            clients = res.Result!;
+            pagination = res.Pagination;
+        }
+        else
+        {
+            var pageIx = 1;
+            clients = new List<Client>();
+
+            while ( true )
+            {
+                var res = await api.ClientListAsync( pageIx, this.PageSize );
+
+                if ( res.IsSuccessful == false )
+                    return console.WriteError( res );
+
+                clients.AddRange( res.Result! );
+
+                if ( res.Pagination!.PageCount == pageIx )
+                    break;
+
+                pageIx++;
+            }
+        }
 
 
         /*
          * 
          */
-        var table = new ConsoleTable( "Id", "Name", "Value", "Country", "VAT #" );
-
-        foreach ( var r in res.Result! )
-            table.AddRow( r.Id, r.Code, r.Name, r.Country, r.TaxNumber );
-
-        table.Write( Format.Minimal );
-
-
-        if ( res.Pagination!.PageCount > 1 )
+        if ( this.EmitJson == false )
         {
-            var p = res.Pagination!;
-            Console.WriteLine( "Page {0}/{1} - Total {2}", p.Page, p.PageCount, p.EntryCount );
+            var table = new ConsoleTable( "Id", "Code", "Name", "Country", "VAT #" );
+
+            foreach ( var r in clients.OrderBy( x => x.Code ) )
+                table.AddRow( r.Id, r.Code, r.Name, r.Country, r.TaxNumber );
+
+            table.Write( Format.Minimal );
+
+            if ( pagination?.PageCount > 1 )
+                Console.WriteLine( "page {0}/{1} - {2} clients", pagination.Page, pagination.PageCount, pagination.EntryCount );
+        }
+        else
+        {
+            var data = clients.OrderBy( x => x.Id ).Select( x => new
+            {
+                Id = x.Id,
+                Code = x.Code,
+                Name = x.Name,
+                Country = x.Country ?? null,
+                TaxNumber = x.TaxNumber ?? null,
+            } );
+
+            var json = JsonSerializer.Serialize( data );
+            Console.Write( json );
         }
 
         return 0;
